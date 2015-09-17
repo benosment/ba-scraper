e7c82d088ff609a8e61ac5bad08c2fef443e840e
Benjamin Osment <benjamin.j.osment@gmail.com>
2015-09-17 07:00:05 -0400

    filling out structure of scraper, moved UT to dict

--- scraper.py
+++ scraper.py
@@ -1,4 +1,69 @@
 import argparse
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+
+def scrape(url):
+    try:
+        html = urlopen(url)
+    except HTTPError:
+        return None
+    soup = BeautifulSoup(html.read())
+    content = {}
+    content['title'] = get_title(soup)
+    content['url'] = url
+    content['img_url'] = get_img_url(soup)
+    content['ingredients'] = get_ingredients(soup)
+    content['directions'] = get_directions(soup)
+    content['servings'] = get_servings(soup)
+    content['total_time'] = get_total_time(soup)
+    content['cooking_time'] = get_cooking_time(soup)
+    return content
+
+
+def get_title(soup):
+    try:
+        title = soup.body.h1
+    except AttributeError:
+        return None
+
+def get_img_url(soup):
+    try:
+        title = soup.body.h1
+    except AttributeError:
+        return None
+
+def get_ingredients(soup):
+    try:
+        title = soup.body.h1
+    except AttributeError:
+        return None
+
+def get_directions(soup):
+    try:
+        title = soup.body.h1
+    except AttributeError:
+        return None
+
+def get_servings(soup):
+    try:
+        title = soup.body.h1
+    except AttributeError:
+        return None
+
+def get_cooking_time(soup):
+    try:
+        title = soup.body.h1
+    except AttributeError:
+        return None
+
+def get_total_time(soup):
+    try:
+        title = soup.body.h1
+    except AttributeError:
+        return None
+
 
 def parse_args():
     pass
--- tests.py
+++ tests.py
@@ -7,36 +7,35 @@ import scraper
 class ScraperUnitTest(unittest.TestCase):
 
     def setUp(self):
-        recipe_url = 'http://www.bonappetit.com/recipe/salt-and-pepper-rib-eye'
-        self.scraped_content = scraper.scrape(recipe_url)
+        self.recipe_url = 'http://www.bonappetit.com/recipe/salt-and-pepper-rib-eye'
+        self.scraped_content = scraper.scrape(self.recipe_url)
 
     def test_title(self):
-        self.assertEqual(self.scraped_content.title, 'Salt-and-Pepper Rib Eye')
+        self.assertEqual(self.scraped_content['title'], 'Salt-and-Pepper Rib Eye')
 
     def test_url(self):
-        self.assertEqual(self.scraped_content.url, self.recipe_url)
+        self.assertEqual(self.scraped_content['url'], self.recipe_url)
 
     def test_img_url(self):
-        self.assertEqual(self.scraped_content.img_url, 'http://www.bonappetit.com/wp-content/uploads/2010/06/salt-and-pepper-rib-eye-940x600.jpg')
+        self.assertEqual(self.scraped_content['img_url'], 'http://www.bonappetit.com/wp-content/uploads/2010/06/salt-and-pepper-rib-eye-940x600.jpg')
 
     def test_ingredients(self):
-        self.assertIn(self.scraped_content.ingredients, 'bone-in rib eye')
-        self.assertIn(self.scraped_content.ingredients, 'kosher salt, divided')
-        self.assertIn(self.scraped_content.ingredients, 'coarsely ground black pepper')
+        self.assertIn(self.scraped_content['ingredients'], 'bone-in rib eye')
+        self.assertIn(self.scraped_content['ingredients'], 'kosher salt, divided')
+        self.assertIn(self.scraped_content['ingredients'], 'coarsely ground black pepper')
 
     def test_directions(self):
-        self.assertIn(self.scraped_content.directions, 'Put steak on a wire rack')
-        self.assertIn(self.scraped_content.directions, 'Build a two-zone fire')
-        self.assertIn(self.scraped_content.directions, 'season with corase sea salt, and serve')
+        self.assertIn(self.scraped_content['directions'], 'Put steak on a wire rack')
+        self.assertIn(self.scraped_content['directions'], 'Build a two-zone fire')
+        self.assertIn(self.scraped_content['directions'], 'season with corase sea salt, and serve')
 
 
     def test_servings(self):
-        self.assertEqual(self.scraped_content.servings, '')
+        self.assertEqual(self.scraped_content['servings'], '')
 
     def test_time(self):
-        self.assertEqual(self.scraped_content.total_time, '')
-        self.assertEqual(self.scraped_content.cooking_Time, '')
-
+        self.assertEqual(self.scraped_content['total_time'], '')
+        self.assertEqual(self.scraped_content['cooking_time'], '')
 
 
 if __name__ == '__main__':
